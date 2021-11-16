9326d7246df4619afad61a0763d72b8fade0f480
StephhyL <Stephanie Li>
2021-11-16 16:30:13 -0500

    linted

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,22 @@
+/**
+adds a class to the target element when the expression is true
+ */
 const addClass = (target, expression, className) => {
-  expression ? target.addClass(className): target.removeClass(className);
+  expression ? target.addClass(className) : target.removeClass(className);
 };
 
 $(document).ready(() => {
-    $("#tweet-text").keyup(function() {
-      const startCount = 140;
-      const tweetLength = $(this).val().length;
-      const count = startCount - tweetLength;
+  $("#tweet-text").keyup(function() {
+    const startCount = 140;
+    const tweetLength = $(this).val().length;
+    const count = startCount - tweetLength;
 
-      //traverses up the DOM tree to form then back down to counter class
-      const counterText = $(this).closest("form").find(".counter")
-      //assigns class "negative" (makes text red) if count is < 0
-      addClass(counterText, count < 0, "negative");
-      
-      //assigns the character displayed on web ("text") to be equal to count
-      counterText.text(count);
-    })
+    // traverses up the DOM tree to form then back down to counter class
+    const counterText = $(this).closest("form").find(".counter");
+    // assigns class "negative" (makes text red) if count is < 0
+    addClass(counterText, count < 0, "negative");
+    
+    // assigns the character displayed on web ("text") to be equal to count
+    counterText.text(count);
+  });
 });
