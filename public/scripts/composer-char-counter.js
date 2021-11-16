4c547bae83cf0ec209747d64acc0b269dddfafbe
StephhyL <Stephanie Li>
2021-11-16 13:00:50 -0500

    changed code to using this and DOM traversal

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,13 +2,18 @@ $(document).ready(() => {
   console.log("document is loaded!");
 })
 
+const vaidateTweet = (tweetLength) => {
+  if (tweetLength <= 140) {
+    return true;
+  }
+  return false;
+}
 
-
-$(() => {
-  let count = 140;
-  $("#tweet-text").keydown(() => {
-    count--;
-    console.log(`you typed something. ${count}`)
-    $(".counter").text(count);
+$(()=> {
+  $("#tweet-text").keyup(function() {
+    const startCount = 140;
+    const tweetLength = $(this).val().length;
+    const count = startCount - tweetLength;
+    $(this).closest(".new-tweet").find(".counter").text(count);
   })
 })
