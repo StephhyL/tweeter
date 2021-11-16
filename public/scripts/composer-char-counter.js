dd38afc64a39d9c7776270cec8f396e051a5fc6c
StephhyL <Stephanie Li>
2021-11-15 20:08:10 -0500

    added base counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
+$(document).ready(() => {
   console.log("document is loaded!");
+})
+
+
+
+$(() => {
+  let count = 140;
+  $("#tweet-text").keydown(() => {
+    count--;
+    console.log(`you typed something. ${count}`)
+    $(".counter").text(count);
+  })
 })
