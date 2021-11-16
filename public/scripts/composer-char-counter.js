74e1b156fb9f070f87f8d21c190801f543ffa9a2
StephhyL <Stephanie Li>
2021-11-16 16:23:29 -0500

    removed unnecessary code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,36 +1,19 @@
+const addClass = (target, expression, className) => {
+  expression ? target.addClass(className): target.removeClass(className);
+};
+
 $(document).ready(() => {
-  console.log("document is loaded!");
-})
+    $("#tweet-text").keyup(function() {
+      const startCount = 140;
+      const tweetLength = $(this).val().length;
+      const count = startCount - tweetLength;
 
-const vaidateTweet = (tweetLength) => {
-  const error = "Please write a tweet that contains no more than 140 characters."
-  if (tweetLength <= 140) {
-    return (null, true);
-  }
-  return (error, null);
-}
-
-/**
-adds a "negative" class to the target element if count is <0*/
-const colourClass = (target, count) => {
-  if (count < 0) {
-    target.addClass("negative");
-  }
-}
-
-
-$(()=> {
-  $("#tweet-text").keyup(function() {
-    const startCount = 140;
-    const tweetLength = $(this).val().length;
-    const count = startCount - tweetLength;
-
-    //traverses up the DOM tree to form then back down to counter class
-    const counterText = $(this).closest("form").find(".counter")
-    //assigns class "negative" (makes text red) if count is < 0
-    colourClass(counterText, count);
-    
-    //assigns the character displayed on web ("text") to be equal to count
-    counterText.text(count);
-  })
-})
+      //traverses up the DOM tree to form then back down to counter class
+      const counterText = $(this).closest("form").find(".counter")
+      //assigns class "negative" (makes text red) if count is < 0
+      addClass(counterText, count < 0, "negative");
+      
+      //assigns the character displayed on web ("text") to be equal to count
+      counterText.text(count);
+    })
+});
