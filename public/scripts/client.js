070ac3557923cb6e92bad038a4b1fbe9f654719b
StephhyL <Stephanie Li>
2021-11-17 14:38:21 -0500

    cleaned up code by removing comments and reordering code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,35 +6,20 @@
 
 // jQuery's document ready function. Everything will run once page is loaded
 
-
 $(()=> {
-  // test data for testing renderTweets(tweets)
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  //   tweets is an array
+  const loadTweets = () => {
+    const data = renderTweets(tweets);
+    //******getting data from /tweets gets an array with 4 object tweets. We can then "render" these tweets. Each tweet goes through create tweet element. Note to self - this is only the GET request of the inital data - for now. Nothing to do with the POST.
+    $.get("/tweets", (tweets)=> {
+      console.log(tweets);
+      renderTweets(tweets);
+    });
+  }
 
-  
+  //calling the loadTweets
+  loadTweets();
+
   // creates a new tweet with all the HTML elements. parameter tweet is an object
   const createTweetElement = (tweet) => {
     
@@ -99,57 +84,27 @@ $(()=> {
     return {error:null, val: true}
   }
 
-  // EXERCISE 2 ********************* FORM VALIDATION
+  // activated when submit button is pressed.
   $("form").submit(function (event) {
     // prevents the default of refreshing the browser
     event.preventDefault();
     // console.log("this.text.value---->", this.text.value);
-
     const tweetLength = this.text.value.length;
-
     const {error, val} = validateTweet(tweetLength);
-
+    // if tweet is not valid, send an alert to the user and abort
     if (error) {
       return alert(error);
     }
 
     // converts/serializes form data into query string
     const serializedData = $(this).serialize();
-   
+
     //post request to server.js
     $.post("/tweets", serializedData, (response) => {
       console.log(serializedData);
-      console.log("hello biss")
+      // console.log("hello biss")
     });
-
   })
-
-//  EXERCISE 3 - FETCHING TWEETS WITH AJAX *********
-//   tweets is an array
-  const loadTweets = () => {
-    const data = renderTweets(tweets);
-    //******getting data from /tweets gets an array with 4 object tweets. We can then "render" these tweets. Each tweet goes through create tweet element. Note to self - this is only the GET request of the inital data - for now. Nothing to do with the POST.
-    $.get("/tweets", (tweets)=> {
-      console.log(tweets);
-      renderTweets(tweets);
-    });
-  }
-  
-  loadTweets();
-
-  // ***EXERCISE 4 ****** VALIDATION
-  // checking if tweet is empty "" or null
-  // if tweet > 140 characters ---> separate error messages
-  // implement validation before sending the form data to the server. ----> 
-  // Notify user, render error message on page ---> alert function
-  // form should not be cleared
-  // form should not be submitted.
-
-
-  // ******PSEUDO CODE
-  // Target is text area. Strings's length > 140 --> too long
-  // text area. "" or null --> pls enter
-  
 
 
 
