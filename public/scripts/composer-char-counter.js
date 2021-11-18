f1a21d4cfffd9ac391fdba44c320a03de6820392
StephhyL <Stephanie Li>
2021-11-18 09:20:18 -0500

    changed akeyup to input, made comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,18 +5,20 @@ const addClass = (target, expression, className) => {
   expression ? target.addClass(className) : target.removeClass(className);
 };
 
+// everything will run when the page has loaded
 $(document).ready(() => {
-  $("#tweet-text").keyup(function() {
+  // activated when the user starts to type a tweet / when there is input
+  $("#tweet-text").on("input", function() {
     const startCount = 140;
     const tweetLength = $(this).val().length;
     const count = startCount - tweetLength;
 
-    // traverses up the DOM tree to form then back down to counter class
+    // traverses up the DOM tree to form element then back down to counter class
     const counterText = $(this).closest("form").find(".counter");
     // assigns class "negative" (makes text red) if count is < 0
     addClass(counterText, count < 0, "negative");
     
-    // assigns the character displayed on web ("text") to be equal to count
+    // assigns the characters-left counter displayed on page to be equal to count
     counterText.text(count);
   });
 });
